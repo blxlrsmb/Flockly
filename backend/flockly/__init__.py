ea134fa1040b74c69986110a6eb47b1032e46e4c
Qijiang Fan <fqj1994@gmail.com>
2014-02-14 17:27:20 +0800

    authpage

--- backend/flockly/__init__.py
+++ backend/flockly/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flockly import config
+
 import flockly.helloworld
+import flockly.authpage
+
---
+++ backend/flockly/authpage.py
@@ -0,0 +1,29 @@
+from flockly import app
+from flockly import config
+from facebook import FacebookAPI, FacebookClientError, GraphAPI
+from flask import redirect, request
+
+
+
+@app.route('/auth')
+def auth():
+    f = FacebookAPI(config.API_KEY, config.API_SECRET, config.REDIRECT_URL)
+    auth_url = f.get_auth_url(scope=[
+        "publish_stream", "read_stream", "status_update", "user_friends"
+        ])
+    return redirect(auth_url)
+
+@app.route('/auth_redirect')
+def auth_redirect():
+    f = FacebookAPI(config.API_KEY, config.API_SECRET, config.REDIRECT_URL)
+    try:
+        access_token = f.get_access_token(request.args.get('code', ''))
+        graph = GraphAPI(access_token['access_token'])
+        id = graph.get('me')['id']
+        save_user(id, access_token['access_token'], int(access_token['expires'] + time.time() - 3600))
+    except FacebookClientError as e:
+        return 'Authorization Failed:' + str(e)
+
+
+def save_user(userid, access_token, access_token_expires):
+    pass
